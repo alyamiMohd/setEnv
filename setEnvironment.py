ff17b1849dd199fca12e434e9cb999f77dbcfc39
Mohammed <130024303+alyamiMohd@users.noreply.github.com>
2023-04-16 09:30:50 +0300

    Update setEnvironment.py

--- setEnvironment.py
+++ setEnvironment.py
@@ -4,7 +4,7 @@ arr = ['code','chromeServer','gitbashLaunch','gitbashServer','gitbashCMD','chrom
 def organizeMe(name='',press='',press2='',press3='',press4='',press5='',write=''):
     if name!='':
         os.system(name)
-    time.sleep(1)
+    time.sleep(0.7)
     pyautogui.keyDown('win')
     pyautogui.press(press)
     if press2 !='':
@@ -16,24 +16,18 @@ def organizeMe(name='',press='',press2='',press3='',press4='',press5='',write=''
     if press5 !='':
         pyautogui.press(press5)
     if write !='':
-        time.sleep(1)
         pyautogui.keyUp('win')
         pyautogui.press('esc')
-        time.sleep(1)
-        pyautogui.write(write, interval=0.01)
-        time.sleep(1)
+        pyautogui.write(write)
         pyautogui.press('enter')
-    time.sleep(1)
     pyautogui.keyUp('win')
     pyautogui.press('esc')
-    time.sleep(1)
 
 def hideMe(write='',name=''):
     if name!='':
         os.system(name)
-    time.sleep(1)
-    pyautogui.write(str(write), interval=0.01)
-    time.sleep(1)
+    time.sleep(0.3) 
+    pyautogui.write(str(write))
     pyautogui.keyDown('win')
     pyautogui.press('down')
     pyautogui.press('down')
@@ -45,17 +39,15 @@ for i in range(len(arr)):
     elif arr[i] =='chromeServer':
         organizeMe('chrome','right', 'up', write='localhost:3000')
     elif arr[i] =='gitbashLaunch':
-        os.startfile(r"C:\Users\aa\Desktop\Git Bash.lnk")
+        os.startfile(r"C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Git\Git Bash.lnk")
         hideMe(write='mongod')
     elif arr[i] =='gitbashCMD':
-        os.startfile(r"C:\Users\aa\Desktop\Git Bash.lnk")
-        organizeMe(press='right',press2='down',write="pwd")
+        os.startfile(r"C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Git\Git Bash.lnk")
+        organizeMe(press='right',press2='down',write=" cd findMyCup/")
     elif arr[i] =='spotify':
         hideMe(name='spotify')
     elif arr[i]=='chromeUdemy':
         organizeMe('chrome', 'right', 'right', write='browserhome')
     elif arr[i]=='gitbashServer':
-        os.startfile(r"C:\Users\aa\Desktop\Git Bash.lnk")
-        time.sleep(1)
+        os.startfile(r"C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Git\Git Bash.lnk")
         organizeMe(press='right',press2='up',press3='right', press4='right',press5='down',write="mongosh")
-
